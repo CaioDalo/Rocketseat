8236126e083b11f1a37f9c335d00ab7ddfd9d0b8
Caio Daló <caio.dalo@hotmail.com>
2024-04-06 13:01:18 -0300

    feat: add avatar component and adjusts

--- ReactJS/01-reactjs-fundamentals/src/components/Comment/Comment.jsx
+++ ReactJS/01-reactjs-fundamentals/src/components/Comment/Comment.jsx
@@ -1,32 +1,40 @@
-import { ThumbsUp, Trash } from 'phosphor-react'
+import { ThumbsUp, Trash } from "phosphor-react";
 
-import styles from './Comment.module.css'
+import styles from "./Comment.module.css";
+import { Avatar } from "../Avatar/Avatar";
 
 export function Comment() {
-  return (
-    <div className={styles.comment}>
-      <img src="https://www.github.com/CaioDalo.png" alt="Avatar" />
+	return (
+		<div className={styles.comment}>
+			<Avatar src="https://www.github.com/CaioDalo.png" hasBorder={false} />
 
-      <div className={styles.commentBox}>
-        <div className={styles.commentContent}>
-          <header>
-            <div className={styles.authorAndTime}>
-              <time title='28 de Fevereiro às 14:58h' dateTime='2024-02-28 14:57:00'>Cerca de 1h atrás</time>
-            </div>
+			<div className={styles.commentBox}>
+				<div className={styles.commentContent}>
+					<header>
+						<div className={styles.authorAndTime}>
+							<time
+								title="28 de Fevereiro às 14:58h"
+								dateTime="2024-02-28 14:57:00"
+							>
+								Cerca de 1h atrás
+							</time>
+						</div>
 
-            <button title='Deletar comentário'>
-              <Trash size={20}/>
-            </button>
-          </header>
+						<button title="Deletar comentário">
+							<Trash size={24} />
+						</button>
+					</header>
 
-          <p>Muito bom Devon, parabéns!! 👏👏</p>
-        </div>
+					<p>Muito bom Devon, parabéns!! 👏👏</p>
+				</div>
 
-        <footer>
-          <ThumbsUp />
-          Aplaudir <span>20</span>
-        </footer>
-      </div>
-    </div>
-  )
+				<footer>
+					<button>
+						<ThumbsUp />
+						Aplaudir <span>20</span>
+					</button>
+				</footer>
+			</div>
+		</div>
+	);
 }
